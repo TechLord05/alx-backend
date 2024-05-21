7ab0f9611e4838518cc7671b1dfdec377de711a9
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-05-21 20:01:30 +0000

    Refactor BasicCache put method for better code readability and maintainability

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,13 +5,17 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ BasicCache is a basic caching system with no limit """
+    
     def put(self, key, item):
         """ Add an item in the cache """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
         if key is None or key not in self.cache_data:
-            return None
+            return
         return self.cache_data[key]
